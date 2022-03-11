817920c5d6ee1b756f2c616db6f73e7d023067df
alhexanderad <alhexanderad@gmail.com>
2022-03-11 17:16:30 -0500

    Se realizo la parte de save en model Car en el campo de code utilizando def save()

--- buyers/__init__.py
+++ buyers/__init__.py
@@ -1,4 +1,1 @@
-from signal import default_int_handler
-
-
 default_app_config = 'buyers.apps.BuyersConfig'
--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
-# Create your models here.
+from django.utils.translation import gettext as _
+from buyers.models import Buyer
+import uuid
+class Car(models.Model):
+  name = models.CharField(_("Nombres"), max_length=200)
+  price= models.PositiveIntegerField(_("Precio"))
+  buyer = models.ForeignKey(Buyer, verbose_name=_("Comprador"), on_delete=models.CASCADE)
+  code = models.CharField(_("codigo"), max_length=10, blank=True)
+  
+  def __str__(self):
+    return f"{self.name}-{self.price}-{self.buyer}"
+  
+  def save(self, *args, **kwargs):
+    if self.code == "":
+      self.code = str(uuid.uuid4()).replace("-","").upper()[:10]
+    return super().save(*args, **kwargs)
