af913febcc6d987d1057c684f1068467440521df
alhexanderad <alhexanderad@gmail.com>
2022-03-11 18:02:19 -0500

    se realizo el post_save con post_save_modify_buyer_and_create_code, para poder grabar buyer model el code de cars

--- cars/signals.py
+++ cars/signals.py
@@ -1,19 +1,29 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from cars.models import Car
 from buyers.models import Buyer
 import uuid
 
-@receiver(pre_save, sender=Car)
-def pre_save_modify_buyer_and_create_code(sender, instance,**kwargs):
-  if instance.code == "":
-    instance.code = str(uuid.uuid4()).replace("-","").upper()[:10]
-    
-  obj = Buyer.objects.get(user= instance.buyer.user)
-  obj.from_signal = True
-  obj.save()
-
 '''pre_save, nos ayuda a guardar de nanera interna algun argunto anticipado 
 para algun campo.
 Tambien podremos grabar buyer model.
 '''
+# @receiver(pre_save, sender=Car)
+# def pre_save_modify_buyer_and_create_code(sender, instance,**kwargs):
+#   if instance.code == "":
+#     instance.code = str(uuid.uuid4()).replace("-","").upper()[:10]
+    
+#   obj = Buyer.objects.get(user= instance.buyer.user)
+#   obj.from_signal = True
+#   obj.save()
+
+
+@receiver(post_save, sender=Car)
+def post_save_modify_buyer_and_create_code(sender, instance, created, **kwargs):
+  if instance.code == "":
+    instance.code = str(uuid.uuid4()).replace("-","").upper()[:10]
+    instance.save()
+    
+  obj = Buyer.objects.get(user= instance.buyer.user)
+  obj.from_signal = True
+  obj.save()
