d3fb168612b8b2e77afe6f2a3964bfb949f44172
alhexanderad <alhexanderad@gmail.com>
2022-03-11 16:44:22 -0500

    Se realizo la parte de post_save en la parte de User

--- buyers/admin.py
+++ buyers/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from buyers.models import Buyer
 
-# Register your models here.
+admin.site.register(Buyer)
--- buyers/models.py
+++ buyers/models.py
@@ -1,3 +1,12 @@
+from math import fabs
+from pyexpat import model
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext as _
 
-# Create your models here.
+class Buyer(models.Model):
+  user = models.OneToOneField(User, verbose_name=_("Usuario"), on_delete=models.CASCADE)
+  from_signal = models.BooleanField(_("Signo"), default=False)
+  
+  def __str__(self):
+    return str(self.user)
