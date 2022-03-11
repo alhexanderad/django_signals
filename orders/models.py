5f1c952a8a1e83b02adc81bf8464a19efab11880
alhexanderad <alhexanderad@gmail.com>
2022-03-11 18:52:35 -0500

    se realizo el m2m_change_cars_order, para poder grabar Order.cars.through

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from cars.models import Car
+from django.utils.translation import gettext as _
 
-# Create your models here.
+class Order(models.Model):
+  name = models.CharField(_("Nombre"), max_length=200)
+  cars = models.ManyToManyField(Car, verbose_name=_("Carros"))
+  total = models.PositiveIntegerField(_("Total"), blank=True, null=True)
+  total_price = models.PositiveIntegerField(_("Precio Total"), blank=True, null=True)
+  active = models.BooleanField(_("Actividad"), default=True)
+  
+  def __str__(self):
+    return str(self.name)
