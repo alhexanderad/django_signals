ad769d6c05bacfb20dd832f78d6b68b0ab9e9b39
alhexanderad <alhexanderad@gmail.com>
2022-03-13 11:56:52 -0500

    Se puede grabar la tabla sale, por medio de orden en por medio de signals

--- sales/models.py
+++ sales/models.py
@@ -1,3 +1,10 @@
+from pyexpat import model
 from django.db import models
-
-# Create your models here.
+from orders.models import Order
+from django.utils.translation import gettext as _
+class Sale(models.Model):
+  order = models.ForeignKey(Order, verbose_name=_("Ordenes"), on_delete=models.CASCADE)
+  amount =  models.PositiveIntegerField(("Monto"), blank=True, null=True )
+  
+  def __str__(self):
+    return str(self.amount)
